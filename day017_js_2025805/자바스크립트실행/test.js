bd9f71192a0c2b8c79e938b7523032905a3531ba
darkroute <entlrqkr@gmail.com>
2025-08-05 17:43:40 +0900

    11일차
    자바스크립트 끝!
    프로젝트 시작!

---
+++ day017_js_2025805/자바스크립트실행/test.js
@@ -0,0 +1,2 @@
+const title = document.getElementById("title");
+        title.textContent = "title ^^";
---
+++ day017_js_2025805/자바스크립트실행/test2.js
@@ -0,0 +1,7 @@
+//dom구축이 완료된 후 
+document.addEventListener("DOMContentLoaded", function(){
+    const title = document.getElementById("title");
+    title.textContent="title ^^";
+});
+
+//dom을 포함된 모든 리소스 완료된후
